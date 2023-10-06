7caae70407644fff502e106967573eb1772313a9
timur <ihave800mmr@yandex.ru>
2023-10-06 16:36:08 +0300

    complete auth module

--- cloud/src/auth/auth.controller.ts
+++ cloud/src/auth/auth.controller.ts
@@ -1,11 +1,24 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Post, UseGuards, Request, Body } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiBody } from '@nestjs/swagger';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { AuthService } from './auth.service';
+import { UserEntity } from '../users/entities/user.entity';
+import { LocalAuthGuard } from './guards/local.guard';
 
 @Controller('auth')
-export class AppController {
-    @UseGuards(AuthGuard())
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @UseGuards(LocalAuthGuard)
     @Post('login')
+    @ApiBody({ type: CreateUserDto })
     async login(@Request() req) {
-        return req.user;
+        return this.authService.login(req.user as UserEntity);
+    }
+
+    @Post('/register')
+    register(@Body() dto: CreateUserDto) {
+        return this.authService.register(dto);
     }
 }
--- cloud/src/auth/auth.service.ts
+++ cloud/src/auth/auth.service.ts
@@ -1,19 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { UserEntity } from '../users/entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private userService: UsersService) {
-    }
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService,
+    ) {}
 
     async validateUser(email: string, password: string): Promise<any> {
-        const user = await this.userService.findByEmail(email)
+        const user = await this.usersService.findByEmail(email);
 
-        if (user && user.password == password) {
-            const { password, ...result } = user
-            return result
+        if (user && user.password === password) {
+            const { password, ...result } = user;
+            return result;
         }
 
-        return null
+        return null;
+    }
+
+    async register(dto: CreateUserDto) {
+        try {
+            const userData = await this.usersService.create(dto);
+
+            return {
+                token: this.jwtService.sign({ id: userData.id }),
+            };
+        } catch (err) {
+            console.log(err);
+            throw new ForbiddenException('Ошибка при регистрации');
+        }
+    }
+
+    async login(user: UserEntity) {
+        return {
+            token: this.jwtService.sign({ id: user.id }),
+        };
     }
 }
